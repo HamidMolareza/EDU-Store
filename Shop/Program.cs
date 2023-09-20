6f3a8d5d550e6baba252222b28f1e95f802b8471
Hamid Molareza <kitm.hamid@gmail.com>
2023-09-21 01:20:37 +0330

    fix: add DbInitializer for all environments

--- Shop/Program.cs
+++ Shop/Program.cs
@@ -19,16 +19,16 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment()) {
     app.UseMigrationsEndPoint();
-
-    using var scope    = app.Services.CreateScope();
-    var       services = scope.ServiceProvider;
-    await DbInitializer.InitializeAsync(services);
 }
 else {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+using var scope    = app.Services.CreateScope();
+var       services = scope.ServiceProvider;
+await DbInitializer.InitializeAsync(services);
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
