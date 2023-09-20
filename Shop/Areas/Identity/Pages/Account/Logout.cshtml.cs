54c97d76c9f2fda026e818075be459a5d5ad536d
Hamid Molareza <kitm.hamid@gmail.com>
2023-09-20 17:16:32 +0330

    chore: remove excess identity pages

--- Shop/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ Shop/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -3,35 +3,31 @@
 
 #nullable disable
 
-using System;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
 
-namespace Shop.Areas.Identity.Pages.Account {
-    public class LogoutModel : PageModel {
-        private readonly SignInManager<IdentityUser> _signInManager;
-        private readonly ILogger<LogoutModel> _logger;
+namespace Shop.Areas.Identity.Pages.Account; 
 
-        public LogoutModel(SignInManager<IdentityUser> signInManager, ILogger<LogoutModel> logger) {
-            _signInManager = signInManager;
-            _logger        = logger;
+public class LogoutModel : PageModel {
+    private readonly SignInManager<IdentityUser> _signInManager;
+    private readonly ILogger<LogoutModel> _logger;
+
+    public LogoutModel(SignInManager<IdentityUser> signInManager, ILogger<LogoutModel> logger) {
+        _signInManager = signInManager;
+        _logger        = logger;
+    }
+
+    public async Task<IActionResult> OnPost(string returnUrl = null) {
+        await _signInManager.SignOutAsync();
+        _logger.LogInformation("User logged out.");
+        if (returnUrl != null) {
+            return LocalRedirect(returnUrl);
         }
-
-        public async Task<IActionResult> OnPost(string returnUrl = null) {
-            await _signInManager.SignOutAsync();
-            _logger.LogInformation("User logged out.");
-            if (returnUrl != null) {
-                return LocalRedirect(returnUrl);
-            }
-            else {
-                // This needs to be a redirect so that the browser performs a new
-                // request and the identity for the user gets updated.
-                return RedirectToPage();
-            }
+        else {
+            // This needs to be a redirect so that the browser performs a new
+            // request and the identity for the user gets updated.
+            return RedirectToPage();
         }
     }
 }
