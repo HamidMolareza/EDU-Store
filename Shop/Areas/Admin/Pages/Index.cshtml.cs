83e318eda1f53618d677500c323aead110123ec1
Hamid Molareza <kitm.hamid@gmail.com>
2023-09-23 09:36:54 +0330

    feat(admin): add cards to admin dashboard

--- Shop/Areas/Admin/Pages/Index.cshtml.cs
+++ Shop/Areas/Admin/Pages/Index.cshtml.cs
@@ -1,8 +1,28 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Shop.Data;
 
 namespace Shop.Areas.Admin.Pages;
 
 public class IndexModel : PageModel {
-    public void OnGet() {
+    private readonly ApplicationDbContext _context;
+    private readonly UserManager<IdentityUser> _userManager;
+
+    public IndexModel(ApplicationDbContext context, UserManager<IdentityUser> userManager) {
+        _context     = context;
+        _userManager = userManager;
+    }
+
+    public Info InfoModel { get; set; } = new();
+
+    public class Info {
+        public int NumOfMessages { get; set; }
+        public int NumOfMembers { get; set; }
+    }
+
+    public async Task OnGetAsync() {
+        InfoModel.NumOfMessages = await _context.Messages.CountAsync();
+        InfoModel.NumOfMembers  = await _userManager.Users.CountAsync();
     }
 }
