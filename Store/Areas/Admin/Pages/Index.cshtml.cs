2fe35afcb720b6ced6b81798b8d34e26df55264c
Hamid Molareza <kitm.hamid@gmail.com>
2023-10-02 11:51:02 +0330

    feat(admin): add carts to dashboard page

--- Store/Areas/Admin/Pages/Index.cshtml.cs
+++ Store/Areas/Admin/Pages/Index.cshtml.cs
@@ -21,6 +21,8 @@ public class IndexModel : PageModel {
         public int NumOfMembers { get; set; }
         public int NumOfCategories { get; set; }
         public int NumOfProducts { get; set; }
+        public int TotalSales { get; set; }
+        public int TotalProductsInCart { get; set; }
     }
 
     public async Task OnGetAsync() {
@@ -28,5 +30,8 @@ public class IndexModel : PageModel {
         InfoModel.NumOfMembers    = await _userManager.Users.CountAsync();
         InfoModel.NumOfCategories = await _context.Categories.CountAsync();
         InfoModel.NumOfProducts   = await _context.Products.CountAsync();
+        InfoModel.TotalSales      = await _context.Carts.Where(cart => cart.IsArchived).SumAsync(cart => cart.Quantity);
+        InfoModel.TotalProductsInCart =
+            await _context.Carts.Where(cart => !cart.IsArchived).SumAsync(cart => cart.Quantity);
     }
 }
