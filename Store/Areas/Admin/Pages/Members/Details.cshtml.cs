0cae7a925e327dc0162574f24485d27749d6e87b
Hamid Molareza <kitm.hamid@gmail.com>
2023-10-02 12:09:38 +0330

    feat(admin): add ProductsBought to members details

--- Store/Areas/Admin/Pages/Members/Details.cshtml.cs
+++ Store/Areas/Admin/Pages/Members/Details.cshtml.cs
@@ -3,14 +3,17 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using Store.Data;
 
 namespace Store.Areas.Admin.Pages.Members;
 
 public class DetailsModel : PageModel {
     private readonly UserManager<IdentityUser> _userManager;
+    private readonly ApplicationDbContext _context;
 
-    public DetailsModel(UserManager<IdentityUser> userManager) {
+    public DetailsModel(UserManager<IdentityUser> userManager, ApplicationDbContext context) {
         _userManager = userManager;
+        _context     = context;
     }
 
     public User UserModel { get; set; }
@@ -22,6 +25,9 @@ public class DetailsModel : PageModel {
         [Display(Name = "ایمیل")] public string? Email { get; set; }
         [Display(Name = "قفل شده؟")] public bool IsLockout { get; set; }
         [Display(Name = "تلفن")] public string? Phone { get; set; }
+
+        [Display(Name = "تعداد محصولات خریداری شده")]
+        public int ProductsBought { get; set; }
     }
 
     public async Task<IActionResult> OnGetAsync(string? id) {
@@ -41,7 +47,9 @@ public class DetailsModel : PageModel {
             Roles     = string.Join(", ", roles),
             Phone     = user.PhoneNumber,
             Email     = user.Email,
-            IsLockout = await _userManager.IsLockedOutAsync(user)
+            IsLockout = await _userManager.IsLockedOutAsync(user),
+            ProductsBought = await _context.Carts.Where(cart => cart.IsArchived && cart.UserId == id)
+                                 .SumAsync(cart => cart.Quantity)
         };
         return Page();
     }
